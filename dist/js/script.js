bb2ca5c63425862fdfe2ea153c78b63779af146e
Cristian Orozco <cristianorozco1984@gmail.com>
2019-07-26 08:04:24 -0500

    create templates

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,52 @@
-  (async function load () {
+(async function load () {
 
-    // adventure
-    // action
-    // horror
-    // drama
-    // animation
-    // comedy
+// adventure
+// action
+// horror
+// drama
+// animation
+// comedy
 
-    async function getData(url) {
-      const response = await fetch(url)
-      const data = await response.json()
-      return data;
-    }
-    
-    const url = 'https://yts.lt/api/v2/list_movies.json?'
-    const adventureList = await getData(url,'genre=horror')
-    console.log(adventureList)
+  async function getData(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data;
+  }
 
-    const advContainer = document.querySelector('#adventure')
+  const urlApi = 'https://yts.lt/api/v2/list_movies.json?genre='
+  const adventureList = await getData(urlApi,'horror')
 
-    const container = document.querySelector('.container')
-    const featContainer = document.querySelector('.featuring')
-    const form = document.querySelector('.search')
-    const overlay = document.querySelector('.overlay')
-    const modal = document.querySelector('.modal')
-    
-    const modalTitle = modal.querySelector('h1')
-    const modalImage = modal.querySelector('img')
-    const modalDescription = modal.querySelector('p')
-    const hideModal = document.querySelector('#hide-modal')
+  // template
+  function movieTemplate(item){
+    return (
+      `<div class="movie">
+        <div class="movie-image">
+          <img src="${item.medium_cover_image}">
+        </div>
+        <h4 class="movie-title">
+          ${item.title}
+        </h4>
+      </div>`
+    )
+  }
 
-  })()
+  adventureList.data.movies.forEach((item) => {
+    const HTMLString = movieTemplate(item)
+    console.log(HTMLString)
+  })
+
+  // Variables
+  const advContainer = document.querySelector('#adventure')
+
+  const container = document.querySelector('.container')
+  const featContainer = document.querySelector('.featuring')
+  const form = document.querySelector('.search')
+  const overlay = document.querySelector('.overlay')
+  const modal = document.querySelector('.modal')
+
+  const modalTitle = modal.querySelector('h1')
+  const modalImage = modal.querySelector('img')
+  const modalDescription = modal.querySelector('p')
+  const hideModal = document.querySelector('#hide-modal')
+
+})()
